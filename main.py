add2650fff86a6d8d4151a7faedb416c29fe5323
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-18 13:21:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def evaluate(args):
     log_dir = get_log_dir(args)
     model_class = get_model_class(args)
 
-    pairs_path = Users\ladet\datasets\RFW\lfw-deepfunneled\Aaron_Eckhart
+    pairs_path = 'Users'\'ladet'\'datasets'\'RFW'\'lfw-deepfunneled'
   
     dataset = LFWPairedDataset(
         dataset_dir, pairs_path, transform_for_infer(model_class.IMAGE_SHAPE))
