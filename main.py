167cfca4a96b8163cac3523420641056dcce1ead
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-18 20:53:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main(args):
 def get_dataset_dir(args):
     home = os.path.expanduser('datasets/RFW/Balancedface/race_per_7000')
     dataset_dir = args.dataset_dir if args.dataset_dir else os.path.join(
-        home, 'Balancedface', 'race_per_7000')
+        home, 'African',)
 
     if not os.path.isdir(dataset_dir):
         os.mkdir(dataset_dir)
