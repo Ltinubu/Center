6553b41f9959cc4a358db1d15ef24a2809de543d
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-18 21:10:33 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -15,7 +15,7 @@ def create_datasets(dataroot, train_val_split=0.9):
         os.mkdir(dataroot)
 
 
-    images_root = os.path.join(dataroot, 'Balancedface', 'race_per_7000', 'African')
+    images_root = os.path.join(dataroot, 'African')
     names = os.listdir(images_root)
     if len(names) == 0:
         raise RuntimeError('Empty dataset')
