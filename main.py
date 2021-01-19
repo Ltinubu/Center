012742fab483edeea47f728c4f52b624dfb3a50e
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-18 21:00:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ def main(args):
 
 
 def get_dataset_dir(args):
-    home = os.path.expanduser('/cmlscratch/dtinubu/datasets/RFW/Balancedface/race_per_7000')
+    home = os.path.expanduser('/cmlscratch/dtinubu/')
     dataset_dir = args.dataset_dir if args.dataset_dir else os.path.join(
-        home, 'African',)
+        home, 'datasets', 'RFW/Balancedface','race_per_7000')
 
     if not os.path.isdir(dataset_dir):
         os.mkdir(dataset_dir)
