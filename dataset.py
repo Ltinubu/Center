7ac6b4b0eca9fbf115dab8468c90f78c2902dcc2
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-19 23:40:22 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -242,10 +242,10 @@ class LFWPairedDataset(PairedDataset):
                     name2, "{}_{:04d}.jpg".format(name2, index2)))
             self.matches.append(match)
 
-def_read_pairs(self, pairs_filename):
+    def _read_pairs(self, pairs_filename):
         pairs = []
         with open(pairs_filename, 'r') as f:
-            for line in f.readlines()[0:]:
+            for line in f.readlines()[1:]:
                 pair = line.strip().split()
                 pairs.append(pair)
         return pairs
