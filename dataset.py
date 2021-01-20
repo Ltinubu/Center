93000c06d3350dc18b36f647c11c0e875716f2d8
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-20 01:07:51 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -246,7 +246,7 @@ class LFWPairedDataset(PairedDataset):
         with open(pairs_filename, 'r') as f:
             for line in f.readlines()[1:]:
                 pair = line.strip().split()
-                f.readlines()[1:]
+                f.readlines()[2:]
                 pair2 = line.strip().split()
                 pair1=pair+pair2
                 print(pair1)
