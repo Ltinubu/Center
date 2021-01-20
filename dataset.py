87c075c50529b210e10538cf0eaceb0eb05da691
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-20 02:54:44 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -252,9 +252,10 @@ class LFWPairedDataset(PairedDataset):
             for line in f.readlines():
                 if i % 2 == 0 :
                     pair_even = line.strip().split()
-                    pair1.append[pair_odd + pair_even]
+                    pair1.append( pair_even)
                 else:
-                    pair_odd = line.strip().split()    
+                    pair_odd = line.strip().split()  
+                    pair1.append( pair_odd)
                 i += 1
           
         
