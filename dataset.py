b5723983651b3abb06bd74d1a4df84229afb51af
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-20 13:47:44 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -228,12 +228,11 @@ class LFWPairedDataset(PairedDataset):
             if pairs[pair][Count_a ] == pairs[pair][Count_b]:
                 match = True
                 index1, name1, index2, name2  = \
-                    int(pair[pair][0]), pairs[pair][1], int(len(pair[pair][1])), int(lens(pair[pair][1]))
-
+                    int(len(pair[pair][0])),  pair[0][1], int(len(pair[pair][1])), pair[0][1]
             else:
                 match = False
                 index1, name1,index2, name2, = \
-                    int(pairs[pair][0]), pair[0][1], int(pair[pair][1]),  int(pairs[pair][1])
+                    int(len(pair[pair][0])), pair[0][1], int(len(pair[pair][1])),  pair[0][1]
                 
             self.image_names_a.append(os.path.join(
                 self.dataroot, 'RFW-deepfunneled',
