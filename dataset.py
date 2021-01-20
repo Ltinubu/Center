b487ec7103b59f2b271ba1137c8ecd5724aff2b6
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-20 02:07:19 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -223,12 +223,12 @@ class LFWPairedDataset(PairedDataset):
         pairs = self._read_pairs(self.pairs_cfg)
 
         for pair in pairs:
-            if ((len(set(pair))==1 ):
+           if ((len(set(pair))==1 ):
                 match = True
                 name1, name2, index1, index2 = \
                     pair[0], pair[0], int(pair[1]), int(pair[2])
 
-            else:
+           else:
                 match = False
                 name1, name2, index1, index2 = \
                     pair[0], pair[2], int(pair[1]), int(pair[2])
