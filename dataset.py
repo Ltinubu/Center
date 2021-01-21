930aa8d4f2b7c4b2911af01bd41852ca45ae6931
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 10:43:19 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -224,7 +224,7 @@ class LFWPairedDataset(PairedDataset):
         pairs = self._read_pairs(self.pairs_cfg)
         for pair in pairs: 
           index = pair.index('<^&>')
-          if pair[:15] == b[index:index+15]:
+          if pair[:15] == pair[index:index+15]:
                 match = True
                 index1, name1, index1, name2,  = \
                 pair[0], int(len(pair[0])), pair[0], int(len(pair[0]))
