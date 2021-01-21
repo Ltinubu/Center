218008e7d8f11ad7da4cdcba697b9b2082331493
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 14:26:38 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -244,7 +244,7 @@ class LFWPairedDataset(PairedDataset):
                 race2=find_pair[5]
                 name2=find_pair[6]
                 index2 = find_pair[7] 
-          self.image_names_a.append(os.path.join( ,/cmlscratch,dtinubu,datasets,RFW,Balancedface,race_per_7000, '/' + race + '/' + name1 + '/' + index1 ))
+          self.image_names_a.append(os.path.join( /cmlscratch,dtinubu,datasets,RFW,Balancedface,race_per_7000, '/' + race + '/' + name1 + '/' + index1 ))
           self.image_names_b.append(os.path.join( self.dataroot, 'RFW-deepfunneled', '/' + race2 + '/' + name2 + '/' + index2 ))
           self.matches.append(match)
    
