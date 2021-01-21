eab455b70a247aef91960278d275f3c00242d7a6
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 12:38:30 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -273,7 +273,7 @@ class LFWPairedDataset(PairedDataset):
         join(sys.argv[2], sys.argv[1])
     
     def _read_pairs(self, pairs_filename):
-        pairs_filename=African
+        pairs_filename= 'African'
         pairs = []
         line1= []
         line2 = []
