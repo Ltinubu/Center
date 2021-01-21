3c7264c4ab0690fc5d7379756ec52e8b26838e50
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 13:55:24 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -142,7 +142,7 @@ class Trainer(object):
         return matches
 
     def persist(self, is_best=False):
-        model_dir = os.path.join(self.log_dir, 'models')
+        model_dir = os.path.join(self.log_dir,arg.save_file_name, 'models')
         if not os.path.isdir(model_dir):
             os.mkdir(model_dir)
         file_name = (
