085456eb516488d3706750b6a9ed9913db6637cf
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 14:17:52 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -262,7 +262,7 @@ class LFWPairedDataset(PairedDataset):
                 if line1[:15] ==line2[:15] :
                   line1 = line1[:-2]
                   line2 = line2[:-2]
-                  pair = line1 + '<^&>/' + line2 
+                  pair = line1 + '/<^&>' + line2 
                   pairs.append(pair)  
                 else:
                   line1 = line1[:-2]
