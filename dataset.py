cb62f827fea779134631e63e915d58d00038134f
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 09:14:24 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -249,27 +249,27 @@ class LFWPairedDataset(PairedDataset):
                 self.matches.append(match)
 
     def join(file_name, join_text):
-    prefix = None
-    current_line = ''
-    for line in open(file_name):
-        if line and line[-1] == '\n':
-            line = line[:-1]
-        try:
-            first_word, rest = line.split('\t', 1)
-        except:
-            first_word = None  # empty line or one without tab
-            rest = line
-        if first_word == prefix:
-            current_line += join_text + rest
-        else:
-            if current_line:
-                print current_line
-            current_line = line
-            prefix = first_word
+        prefix = None
+        current_line = ''
+        for line in open(file_name):
+            if line and line[-1] == '\n':
+                line = line[:-1]
+            try:
+                first_word, rest = line.split('\t', 1)
+            except:
+                first_word = None  # empty line or one without tab
+                rest = line
+            if first_word == prefix:
+                current_line += join_text + rest
+            else:
+                if current_line:
+                    print current_line
+                current_line = line
+                prefix = first_word
 
-    if current_line:  # do the last line(s)
-        print current_line
-    join(sys.argv[2], sys.argv[1])
+        if current_line:  # do the last line(s)
+            print current_line
+        join(sys.argv[2], sys.argv[1])
     
     def _read_pairs(self, pairs_filename):
         join(pairs_filename,[:15] )
