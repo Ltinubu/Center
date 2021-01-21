f5f9dbf00d7b1f7d5d280e647423e85fcf4e8acc
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 09:39:08 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -274,8 +274,8 @@ class LFWPairedDataset(PairedDataset):
     def _read_pairs(self, pairs_filename):
         pairs = []
         with open(pairs_filename, 'r') as f:
-        for line in f.readlines()[1:]:
-           pair = line.strip().split()
+            for line in f.readlines()[1:]:
+            pair = line.strip().split()
                 pairs.append(pair)
          lines = [item.strip() for item in input.readlines() if not item == ""]
          templist = []
