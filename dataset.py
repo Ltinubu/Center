a759406ecf4a45fe1f3dd09557e3ee7f0615cff9
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-21 23:57:17 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -246,7 +246,7 @@ class LFWPairedDataset(PairedDataset):
           files=os.listdir(path)
           d=random.choice(files)
           path_1=os.listdir
-          self.image_names_b.append(/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + race2 + '/' + name2 +'/'+ path_1)
+          self.image_names_a.append(/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + race2 + '/' + name2 +'/'+ path_1)
           self.matches.append(match)
           path2= '/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + race2 + '/' + name2 +'/'
           files2=os.listdir(path2)
