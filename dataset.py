5ee89eaf230364b6126387f9f37c9fee336cfcbe
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-31 17:50:14 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -30,6 +30,7 @@ def create_datasetsAF(dataroot, train_val_split=0.9, ):
     af_training_set =[]
     af_validation_set =[]
     
+  
     for klass, name in enumerate(names_af):
         def add_class(image):
             image_path = os.path.join(images_root, name, image)
@@ -46,6 +47,31 @@ def create_datasetsAF(dataroot, train_val_split=0.9, ):
                     images_of_person[floor(total * train_val_split):])
         
         return af_training_set, af_validation_set, len(names_af)
+    
+    def perm(n, np):
+    p = []
+    d = 1
+    for i in range(n):
+        if np % 2 == 0:
+            p.append(d)
+            d = 1
+        else:
+            d += 1
+        np //= 2
+    return p
+
+
+def test(ex_n):
+    for ex_p in range(2 ** (ex_n - 1)):
+        p = perm(ex_n, ex_p)
+        print(len(p), p)
+
+
+def randperm(n):
+    np = random.randint(0, 2 ** (n - 1))
+    return perm(n, np)
+
+print(randperm(10))
         
     
 
