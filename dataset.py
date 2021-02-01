cbdcedc4846642c3163d55351653435408b02095
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-31 20:44:46 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -237,10 +237,10 @@ class LFWPairedDataset(PairedDataset):
                 name1, name2, index1, index2 = \
                     pair[0], pair[0], pair[0], pair[0]
         lol = random.choice(os.listdir('/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' +'/' + name1 +'/'))
-        imagek=os.path('/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + name1 + '/' + name1 +'/'+ lol)
+        imagek=os.path('/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + name1 + '/'+ lol)
         self.image_names_a.append(imagek)
         lol_1 = random.choice(os.listdir('/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + '/' + name2 +'/'))
-        imager=os.path('/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + name1 + '/' + name1 +'/'+ lol_1)
+        imager=os.path('/cmlscratch/dtinubu/datasets/RFW/eve_set/test/data/' + name1 +'/'+ lol_1)
         self.image_names_b.append(imager)
     
     def _read_pairs(self, pairs_filename):
