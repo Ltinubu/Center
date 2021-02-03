36a36a56e7815ddc9ebc3c7f52d7da2eacce75ea
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-02-03 18:15:44 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -190,7 +190,7 @@ class Dataset(data.Dataset):
         return len(self.datasets)
 
     def __getitem__(self, index):
-        image = image_loader(self.datasets[index])
+        image = image_loader(self.datasets[index][0])
         if self.transform:
             image = self.transform(image)
         return (image, self.datasets[index][1], self.datasets[index][2])
