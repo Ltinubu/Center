83b0445a203f82e7a0cde520d61d295937be3ea9
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-02-03 13:43:20 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -227,7 +227,7 @@ class LFWPairedDataset(PairedDataset):
         pairs = self._read_pairs(self.pairs_cfg)
 
         for pair in pairs:
-            asad=2
+           asad=2
            race1= os.listdir(pair)
            name1=find_pair[2]
            index1 = find_pair[3] 
