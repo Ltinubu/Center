ea8edb93604e2745cee3202e4666897cd434d747
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-02-03 12:30:14 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -247,7 +247,7 @@ class LFWPairedDataset(PairedDataset):
     def _read_pairs(self, pairs_filename):
         pairs = []
         with open(pairs_filename, 'r') as f:
-            for line in f.readlines()[1:]:
+            for line in f.readlines():
                 pair = line.strip().split()
                 pairs.append(pair)
         return pairs
