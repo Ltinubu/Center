866b5ae852b3383937db12fe9323f262392abbe8
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-02-03 19:24:10 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -227,8 +227,8 @@ class PairedDataset(data.Dataset):
         return len(self.matches)
 
     def __getitem__(self, index):
-        return (self.transform(self.loader(self.image_names_a[index])),
-                self.transform(self.loader(self.image_names_b[index])),
+        return (self.transform(self.loader(self.image_names_a[index][1])),
+                self.transform(self.loader(self.image_names_b[index][2])),
                 self.matches[index])
 
     def _prepare_dataset(self):
