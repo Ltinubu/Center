c40f9bffa5b243ee9526e708ae2a241d862f10cd
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-02-10 17:39:16 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -250,11 +250,11 @@ class LFWPairedDataset(PairedDataset):
                     pair[0], pair[2], int(pair[1]), int(pair[3])
 
             self.image_names_a.append(os.path.join(
-                    self.dataroot, 'RFW-deepfunneled',
+                    self.dataroot,
                     name1, "{}_{:04d}.jpg".format(name1, index1)))
 
             self.image_names_b.append(os.path.join(
-                    self.dataroot, 'RFW-deepfunneled',
+                    self.dataroot,
                     name2, "{}_{:04d}.jpg".format(name2, index2)))
             self.matches.append(match)
 
